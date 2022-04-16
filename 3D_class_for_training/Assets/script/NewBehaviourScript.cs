147e5d5e7498f4d1802379c273c9bd8a8b40064a
TRani-versauen <96644752+TRani-versauen@users.noreply.github.com>
2022-04-16 15:05:54 +0800

    3:05

---
+++ 3D_class_for_training/Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// TRani 的命名空間
+/// </summary>
+namespace TRani
+{
+    /// <summary>
+    /// 攻擊系統
+    /// 1.切換武器
+    /// 2.攻擊動畫
+    /// 3.刀光特效
+    /// 4.攻擊判定
+    /// </summary>
+    public class NewBehaviourScript : MonoBehaviour
+    {
+
+    }
+}
+
